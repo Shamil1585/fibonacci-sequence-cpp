89bc4f56a4b628d0b72085e372fbec2ff8415347
Shamil1585 <userrr20171011@gmail.com>
2025-03-19 17:36:31 +0400

    Первая версия программы: вывод 'Hello, World!'

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -1,16 +1,6 @@
 #include <iostream>
 
-void fib(int n) {
-    int a = 0, b = 1;
-    for (int i = 0; i < n; i++) {
-        std::cout << a << std::endl;
-        int temp = a;
-        a = b;
-        b = temp + b;
-    }
-}
-
 int main() {
-    fib(10);
+    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
