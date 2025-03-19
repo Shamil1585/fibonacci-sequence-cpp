767742335bbd1e27ebcfc026c2e6529b3ab02891
Shamil1585 <userrr20171011@gmail.com>
2025-03-19 17:37:28 +0400

    Исправлена функция fib(n), теперь последовательность начинается с 0, 1

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 
 int fib(int n) {
-    if (n <= 2) {
+    if (n == 1) {
+        return 0;
+    } else if (n == 2) {
         return 1;
     }
     return fib(n - 1) + fib(n - 2);
